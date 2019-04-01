d804c965bc6f80106118643b6ec2cd3788e7fdf2
cenamorado <isaaceverywere@gmail.com>
2019-03-31 22:40:48 -0600

    facebook api

--- themes/alterego/assets/js/app.js
+++ themes/alterego/assets/js/app.js
@@ -1,1 +1,279 @@
-!function(e){var t={};function a(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,a),o.l=!0,o.exports}a.m=e,a.c=t,a.d=function(e,t,n){a.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:n})},a.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return a.d(t,"a",t),t},a.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},a.p="/",a(a.s=0)}([function(e,t,a){a(1),e.exports=a(7)},function(e,t,a){a(2),a(3),a(4),a(5),a(6)},function(e,t){var a=$(".accordion").children().first(),n=$(a).find(".reveal").prop("scrollHeight")+"px";$(a).parent().toggleClass("active"),$(a).find(".reveal").css("max-height",n),$(".accordion").children().first().addClass("active"),$(".accordion-item h3").on("click",function(){panelH=$(this).siblings(".reveal").prop("scrollHeight")+"px",$(".accordion-item").removeClass("active"),$(".reveal").css("max-height",0),$(this).parent().toggleClass("active"),$(this).siblings(".reveal").css("max-height",panelH)})},function(e,t){$(".modal-toggle").on("click",function(e){e.preventDefault();var t=$(this).attr("href");$(t).toggleClass("active")}),$(".modal-background").on("click",function(){$(this).parent().removeClass("active")}),$(".modal .close").on("click",function(e){e.preventDefault(),$(this).parent().parent().removeClass("active")})},function(e,t){var a=$(".tabs").children().first(),n=$(".tabs-content").children().first();$(a).addClass("active"),$(n).addClass("active"),$(".tab-link").on("click",function(e){e.preventDefault();var t=$(this).attr("href");$(".tab-link").removeClass("active"),$(this).addClass("active"),$(".tab-content").removeClass("active"),$(t).addClass("active"),$(".tabs-container").animate({scrollLeft:"+=50"},500)})},function(e,t){$(document).ready(function(){(new WOW).init()}),function(e){e.fn.customerPopup=function(e,t,a,n){e.preventDefault(),t=t||"500",a=a||"400",strResize=n?"yes":"no";var o=void 0!==this.attr("title")?this.attr("title"):"Social Share",i="width="+t+",height="+a+",resizable="+strResize;window.open(this.attr("href"),o,i).focus()},e(document).ready(function(e){e(".customer.share").on("click",function(t){e(this).customerPopup(t)})})}(jQuery)},function(e,t){$(".menu").click(function(e){$(".navbar-links").toggleClass("active")}),$(".open-overlay").click(function(){$(".open-overlay").css("pointer-events","none");var e=$(".overlay-navigation"),t=$(".bar-top"),a=$(".bar-middle"),n=$(".bar-bottom");e.toggleClass("overlay-active"),e.hasClass("overlay-active")?(t.removeClass("animate-out-top-bar").addClass("animate-top-bar"),a.removeClass("animate-out-middle-bar").addClass("animate-middle-bar"),n.removeClass("animate-out-bottom-bar").addClass("animate-bottom-bar"),e.removeClass("overlay-slide-up").addClass("overlay-slide-down"),e.velocity("transition.slideLeftIn",{duration:300,delay:0,begin:function(){$("nav ul a li").velocity("transition.perspectiveLeftIn",{stagger:150,delay:0,complete:function(){$("nav ul a li p").velocity({opacity:[1,0]},{delay:10,duration:140}),$(".open-overlay").css("pointer-events","auto")}})}})):($(".open-overlay").css("pointer-events","none"),t.removeClass("animate-top-bar").addClass("animate-out-top-bar"),a.removeClass("animate-middle-bar").addClass("animate-out-middle-bar"),n.removeClass("animate-bottom-bar").addClass("animate-out-bottom-bar"),e.removeClass("overlay-slide-down").addClass("overlay-slide-up"),$("nav ul a li").velocity("transition.perspectiveRightOut",{stagger:150,delay:0,complete:function(){e.velocity("transition.fadeOut",{delay:0,duration:300,complete:function(){$("nav ul a li p").velocity({opacity:[0,1]},{delay:0,duration:50}),$(".open-overlay").css("pointer-events","auto")}})}}))})},function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+module.exports = __webpack_require__(7);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/*-----------------------------------------------------------------------------------
+ *  Derechos de autor (c) Cinco 04 Digital SA de CV. Todos los derechos reservados.
+ *---------------------------------------------------------------------------------*/
+__webpack_require__(2);
+__webpack_require__(3);
+__webpack_require__(4);
+__webpack_require__(5);
+__webpack_require__(6);
+/*-----------------------------------------------------------------------------------
+ *  Derechos de autor (c) Cinco 04 Digital SA de CV. Todos los derechos reservados.
+ *---------------------------------------------------------------------------------*/
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+var firstPanel = $('.accordion').children().first();
+var firstPanelH = $(firstPanel).find('.reveal').prop('scrollHeight') + 'px';
+
+$(firstPanel).parent().toggleClass('active');
+$(firstPanel).find('.reveal').css('max-height', firstPanelH);
+
+$('.accordion').children().first().addClass('active');
+$('.accordion-item h3').on('click', function () {
+	//get panel height
+	panelH = $(this).siblings('.reveal').prop('scrollHeight') + 'px';
+
+	//remove active from all
+	$('.accordion-item').removeClass('active');
+	$('.reveal').css('max-height', 0);
+
+	//add to current
+	$(this).parent().toggleClass('active');
+	$(this).siblings('.reveal').css('max-height', panelH);
+});
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+$('.modal-toggle').on('click', function (ev) {
+	ev.preventDefault();
+
+	var target = $(this).attr('href');
+	$(target).toggleClass('active');
+});
+$('.modal-background').on('click', function () {
+	$(this).parent().removeClass('active');
+});
+$('.modal .close').on('click', function (ev) {
+	ev.preventDefault();
+
+	$(this).parent().parent().removeClass('active');
+});
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+var firstTabLink = $('.tabs').children().first();
+var firstTabContent = $('.tabs-content').children().first();
+
+$(firstTabLink).addClass('active');
+$(firstTabContent).addClass('active');
+
+$('.tab-link').on('click', function (ev) {
+	ev.preventDefault();
+
+	//define target panel
+	var target = $(this).attr('href');
+
+	//remove active from all
+	$('.tab-link').removeClass('active');
+	$(this).addClass('active');
+
+	//add active to current
+	$('.tab-content').removeClass('active');
+	$(target).addClass('active');
+
+	//scroll
+	$('.tabs-container').animate({ scrollLeft: '+=50' }, 500);
+});
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+$(document).ready(function () {
+	new WOW().init();
+})
+//share Buttons
+;(function ($) {
+
+	/**
+  * jQuery function to prevent default anchor event and take the href * and the title to make a share popup
+  *
+  * @param  {[object]} e           [Mouse event]
+  * @param  {[integer]} intWidth   [Popup width defalut 500]
+  * @param  {[integer]} intHeight  [Popup height defalut 400]
+  * @param  {[boolean]} blnResize  [Is popup resizeabel default true]
+  */
+	$.fn.customerPopup = function (e, intWidth, intHeight, blnResize) {
+
+		// Prevent default anchor event
+		e.preventDefault();
+
+		// Set values for window
+		intWidth = intWidth || '500';
+		intHeight = intHeight || '400';
+		strResize = blnResize ? 'yes' : 'no';
+
+		// Set title and open popup with focus on it
+		var strTitle = typeof this.attr('title') !== 'undefined' ? this.attr('title') : 'Social Share',
+		    strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,
+		    objWindow = window.open(this.attr('href'), strTitle, strParam).focus();
+	};
+
+	/* ================================================== */
+
+	$(document).ready(function ($) {
+		$('.customer.share').on("click", function (e) {
+			$(this).customerPopup(e);
+		});
+	});
+})(jQuery);
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+$('.menu').click(function (event) {
+	$('.navbar-links').toggleClass('active');
+});
+$('.open-overlay').click(function () {
+	$('.open-overlay').css('pointer-events', 'none');
+	var overlay_navigation = $('.overlay-navigation'),
+	    top_bar = $('.bar-top'),
+	    middle_bar = $('.bar-middle'),
+	    bottom_bar = $('.bar-bottom');
+
+	overlay_navigation.toggleClass('overlay-active');
+	if (overlay_navigation.hasClass('overlay-active')) {
+
+		top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
+		middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
+		bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
+		overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down');
+		overlay_navigation.velocity('transition.slideLeftIn', {
+			duration: 300,
+			delay: 0,
+			begin: function begin() {
+				$('nav ul a li').velocity('transition.perspectiveLeftIn', {
+					stagger: 150,
+					delay: 0,
+					complete: function complete() {
+						$('nav ul a li p').velocity({
+							opacity: [1, 0]
+						}, {
+							delay: 10,
+							duration: 140
+						});
+						$('.open-overlay').css('pointer-events', 'auto');
+					}
+				});
+			}
+		});
+	} else {
+		$('.open-overlay').css('pointer-events', 'none');
+		top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+		middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+		bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+		overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up');
+		$('nav ul a li').velocity('transition.perspectiveRightOut', {
+			stagger: 150,
+			delay: 0,
+			complete: function complete() {
+				overlay_navigation.velocity('transition.fadeOut', {
+					delay: 0,
+					duration: 300,
+					complete: function complete() {
+						$('nav ul a li p').velocity({
+							opacity: [0, 1]
+						}, {
+							delay: 0,
+							duration: 50
+						});
+						$('.open-overlay').css('pointer-events', 'auto');
+					}
+				});
+			}
+		});
+	}
+});
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+/******/ ]);
