ba29a50673112836a28b2d9c1213fba0e691ec58
Christopher Enamorado <isaaceverywere@gmail.com>
2019-06-24 11:52:02 -0600

    cambio en momentaneo

--- themes/alterego/_dev/js/ready.js
+++ themes/alterego/_dev/js/ready.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 })
 //share Buttons
 ;(function($){
-  
+
 	/**
 	 * jQuery function to prevent default anchor event and take the href * and the title to make a share popup
 	 *
@@ -13,28 +13,28 @@ $(document).ready(function() {
 	 * @param  {[boolean]} blnResize  [Is popup resizeabel default true]
 	 */
 	$.fn.customerPopup = function (e, intWidth, intHeight, blnResize) {
-	  
+
 	  // Prevent default anchor event
 	  e.preventDefault();
-	  
+
 	  // Set values for window
 	  intWidth = intWidth || '500';
 	  intHeight = intHeight || '400';
 	  strResize = (blnResize ? 'yes' : 'no');
-  
+
 	  // Set title and open popup with focus on it
 	  var strTitle = ((typeof this.attr('title') !== 'undefined') ? this.attr('title') : 'Social Share'),
-		  strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,            
+		  strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,
 		  objWindow = window.open(this.attr('href'), strTitle, strParam).focus();
 	}
-	
+
 	/* ================================================== */
-	
+
 	$(document).ready(function ($) {
 	  $('.customer.share').on("click", function(e) {
 		$(this).customerPopup(e);
 	  });
 	});
-	  
+
 	}(jQuery));
-	
+
